b879a71be78cd0bae1fd1020e7daf1f4c16a1887
AlanSeyfir <seyfirdeveloper@gmail.com>
2024-08-16 20:42:26 -0600

    Fix import

--- app/(tabs)/about.js
+++ app/(tabs)/about.js
@@ -18,29 +18,16 @@ export default function About() {
           </StyledPressable>
         </Link>
         <Text className="text-white font-bold mb-8 text-2xl">
-          Sobre el proyecto
+          About the project
         </Text>
         <Text className="text-white text-white/90 mb-4">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
+          Application for iOS and Android that collects and presents information
+          from Metacritic using React Native.
         </Text>
         <Text className="text-white text-white/90 mb-4">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-        </Text>
-        <Text className="text-white text-white/90 mb-4">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five
+          Integration with the Metacritic API to obtain real-time data on game
+          ratings and information, allowing users to access up-to-date, relevant
+          information and using expo for faster development.
         </Text>
       </ScrollView>
     </Screen>
